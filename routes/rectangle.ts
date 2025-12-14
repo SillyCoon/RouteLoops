942894854536201121cfce46c3b0e3091af3896b
SillyCoon <self-councious@proton.me>
2025-12-14 10:53:06 +0900

    rectangle to ts

--- routes/rectangle.ts
+++ routes/rectangle.ts
@@ -8,7 +8,7 @@ const RECT_MAX_RATIO = 5; // max height:width ratio
 const RECT_MIN_RATIO = 1 / RECT_MAX_RATIO; // min height:width ratio
 const DELTA_RATIO = RECT_MAX_RATIO - RECT_MIN_RATIO;
 
-const calculateSquareMeasurements = (length) => {
+const calculateSquareMeasurements = (length: number) => {
 	const ratio = Math.random() * DELTA_RATIO + RECT_MIN_RATIO;
 	const width = length / (2 * ratio + 2);
 	const height = width * ratio;
@@ -23,7 +23,12 @@ const calculateSquareMeasurements = (length) => {
 	};
 };
 
-export function rectangleRoute(BaseLocation, length, travelHeading, rotation) {
+export function rectangleRoute(
+	BaseLocation: { lat: number; lng: number },
+	length: number,
+	travelHeading: number,
+	rotation: "clockwise" | "counterclockwise",
+) {
 	const { width, height, diagonal, theta } =
 		calculateSquareMeasurements(length);
 
